2d35cb17bb70ea8a38f81ec7060ecc2926db1c15
Kamoool <xkam555@gmail.com>
2021-05-29 14:06:34 +0200

    Login-logout implemented

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <h1>Twój e-mail to {{ email }}</h1>
+    <input type="text" v-model="email" />
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi.</div> -->
+
+    <h1>Witaj w systemie do zapisów na zajęcia</h1>
+    <div v-if="!logged">
+      Zaloguj się e-mailem
+      <input type="text" v-model="email" />
+      <button @click="logged = true">ZALOGUJ</button>
+    </div>
+    <div v-else-if="logged">
+      <div>Witaj {{ email }}!</div>
+      <div>
+        <button @click="logged = false">Wyloguj</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      email: "",
+      password: "",
+      logged: false,
+    };
+  },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    },
+    login() {
+      logged: true;
+    },
+    logout() {
+      logged = false;
+    },
+  },
+};
 </script>
 
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
